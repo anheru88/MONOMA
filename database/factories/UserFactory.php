a1a66fe97e76cdd576db4528e68b33013626da2b
Angel Jimenez <ajimenez@digitalcaribe.com.co>
2023-11-21 12:22:17 -0500

    Add extra fields to UserFactory

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -25,6 +25,9 @@ class UserFactory extends Factory
             'email' => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
+            'last_login' => now(),
+            'is_active' => fake()->boolean,
+            'role' => fake()->randomElement(['manager', 'agent']),
             'remember_token' => Str::random(10),
         ];
     }
